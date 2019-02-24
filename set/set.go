513ef9415e14306fec27ff659e9caeed4fd2b853
Alex Tan Hong Pin <alextan220990@gmail.com>
2019-02-24 18:44:48 +0800

    chore(set): create file

---
+++ set/set.go
@@ -0,0 +1,62 @@
+package set
+
+import (
+	"sync"
+)
+
+type (
+	Set interface {
+		Has(key interface{}) bool
+		Add(key interface{})
+		Remove(key interface{})
+	}
+	SetImpl struct {
+		value map[interface{}]struct{}
+	}
+)
+
+func New() *SetImpl {
+	return &SetImpl{make(map[interface{}]struct{})}
+}
+
+func (s *SetImpl) Add(key interface{}) {
+	s.value[key] = struct{}{}
+}
+
+func (s *SetImpl) Has(key interface{}) bool {
+	_, exist := s.value[key]
+	return exist
+}
+
+func (s *SetImpl) Remove(key interface{}) {
+	delete(s.value, key)
+}
+
+type ConcurrentSet struct {
+	sync.RWMutex
+	value map[interface{}]struct{}
+}
+
+func NewConcurrent() *ConcurrentSet {
+	value := make(map[interface{}]struct{})
+	return &ConcurrentSet{value: value}
+}
+
+func (c *ConcurrentSet) Add(key interface{}) {
+	c.Lock()
+	c.value[key] = struct{}{}
+	c.Unlock()
+}
+
+func (c *ConcurrentSet) Has(key interface{}) bool {
+	c.RLock()
+	_, exist := c.value[key]
+	c.RUnlock()
+	return exist
+}
+
+func (c *ConcurrentSet) Remove(key interface{}) {
+	c.Lock()
+	delete(c.value, key)
+	c.Unlock()
+}
