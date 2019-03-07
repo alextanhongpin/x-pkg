a33ef338e03978bdbc2929cc7e43b8b79c270b4f
alextanhongpin <alextan220990@gmail.com>
2019-03-08 01:00:39 +0800

    chore: add comment

--- hmac256/hmac256.go
+++ hmac256/hmac256.go
@@ -6,6 +6,7 @@ import (
 )
 
 // https://console.bluemix.net/docs/services/cloud-object-storage/hmac/hmac-signature.html#constructing-an-hmac-signature
+// https://dev.to/mathijspim/hmac-authentication-better-protection-for-your-api-4e0
 
 func ValidMAC(message, messageMAC, key []byte) bool {
 	mac := hmac.New(sha256.New, key)
