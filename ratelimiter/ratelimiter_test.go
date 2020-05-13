aaeda999d30b1299c8170e84eafc9f209362f99d
alextanhongpin <alextan220990@gmail.com>
2020-05-13 19:23:15 +0800

    test(ratelimiter): add test

--- ratelimiter/ratelimiter_test.go
+++ ratelimiter/ratelimiter_test.go
@@ -2,6 +2,7 @@ package ratelimiter_test
 
 import (
 	"fmt"
+	"testing"
 	"time"
 
 	"github.com/alextanhongpin/pkg/ratelimiter"
@@ -29,4 +30,26 @@ func Example_in_memory() {
 	time.Sleep(2 * time.Second)
 	ok = rl.Allow("user_1")
 	fmt.Println("allowed?", ok)
+}
+
+func TestRateLimiter(t *testing.T) {
+
+	var (
+		frequency   = ratelimiter.Per(time.Second, 5)
+		burst       = 5
+		inactiveTTL = time.Second
+	)
+	rl, cancel := ratelimiter.New(frequency, burst, inactiveTTL)
+	defer cancel()
+
+	// Use all quota.
+	ok := rl.Allow("user_1")
+	ok = rl.Allow("user_1")
+	ok = rl.Allow("user_1")
+	ok = rl.Allow("user_1")
+	ok = rl.Allow("user_1")
+	ok = rl.Allow("user_1")
+	if ok {
+		t.Errorf("expected false, got true")
+	}
 }
