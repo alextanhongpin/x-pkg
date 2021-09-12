742e69ed6da54847aec6e27e1de0a7cc08a256cd
alextanhongpin <alextan220990@gmail.com>
2021-09-12 12:44:30 +0800

    refactor: simplify stringcase

--- stringcase/stringcase.go
+++ stringcase/stringcase.go
@@ -1,70 +1,26 @@
 package stringcase
 
 import (
-	"fmt"
-	"log"
 	"regexp"
 	"strings"
-	"unicode/utf8"
 )
 
 var (
-	splitRe  *regexp.Regexp
-	camelRe  *regexp.Regexp
-	trailRe  *regexp.Regexp
-	repeatRe *regexp.Regexp
+	sepRe   *regexp.Regexp
+	camelRe *regexp.Regexp
 )
 
 func init() {
-	var err error
-	splitRe, err = regexp.Compile("(i?)[^a-z0-9]+[a-z0-9]")
-	if err != nil {
-		log.Fatal(err)
-	}
-	camelRe, err = regexp.Compile("[a-z][A-Z]")
-	if err != nil {
-		log.Fatal(err)
-	}
-	trailRe, err = regexp.Compile("(?i)^[^a-z0-9]+|[^a-z0-9]+$")
-	if err != nil {
-		log.Fatal(err)
-	}
-	repeatRe, err = regexp.Compile("(?i)[^a-z0-9]+")
-	if err != nil {
-		log.Fatal(err)
-	}
+	sepRe = regexp.MustCompile(`(?i)([^a-z0-9]+)`)
+	camelRe = regexp.MustCompile(`([a-z])([A-Z0-9])`)
 }
 
 type StringCaser func(string) string
 
-var KebabCase = Pipe(identity, camelToKebab, replaceRepeatingSpecialCharactersWithHyphen, lowerAll)
-var PascalCase = Pipe(identity, normalizeToKebab, kebabToPascal)
-var CamelCase = Pipe(identity, normalizeToKebab, kebabToPascal, lowerFirst)
-var SnakeCase = Pipe(identity, normalizeToKebab, kebabToSnake)
-
-func normalizeToKebab(next StringCaser) StringCaser {
-	return func(s string) string {
-		s = KebabCase(s)
-		return next(s)
-	}
-}
-
-func camelToKebab(next StringCaser) StringCaser {
-	return func(s string) string {
-		s = camelRe.ReplaceAllStringFunc(s, func(s string) string {
-			h, size := utf8.DecodeRuneInString(s)
-			if size == 0 {
-				return ""
-			}
-			t, size := utf8.DecodeRuneInString(s[size:])
-			if size == 0 {
-				return fmt.Sprintf("%c", h)
-			}
-			return fmt.Sprintf("%c-%c", h, t)
-		})
-		return next(s)
-	}
-}
+var KebabCase = Pipe(identity, toKebab)
+var PascalCase = Pipe(identity, toKebab, kebabToPascal)
+var CamelCase = Pipe(identity, toKebab, kebabToPascal, lowerFirst)
+var SnakeCase = Pipe(identity, toKebab, kebabToSnake)
 
 func kebabToPascal(next StringCaser) StringCaser {
 	return func(s string) string {
@@ -89,17 +45,14 @@ func kebabToSnake(next StringCaser) StringCaser {
 	}
 }
 
-func replaceRepeatingSpecialCharactersWithHyphen(next StringCaser) StringCaser {
+func toKebab(next StringCaser) StringCaser {
 	return func(s string) string {
-		s = repeatRe.ReplaceAllString(s, "-")
-		s = trailRe.ReplaceAllString(s, "")
+		s = camelRe.ReplaceAllString(s, "$1 $2")
+		s = sepRe.ReplaceAllString(s, " ")
+		s = strings.TrimSpace(s)
+		s = strings.ReplaceAll(s, " ", "-")
+		s = strings.ToLower(s)
 		return next(s)
-	}
-}
-
-func lowerAll(next StringCaser) StringCaser {
-	return func(s string) string {
-		return next(strings.ToLower(s))
 	}
 }
 
